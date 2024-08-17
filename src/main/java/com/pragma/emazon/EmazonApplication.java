398c466fc97d29eec816e255de1185f6c1f88f72
Alexander940 <alexanderecheverry@outlook.es>
2024-08-16 20:32:05 -0500

    first commit and model-entity first hu

---
+++ src/main/java/com/pragma/emazon/EmazonApplication.java
@@ -0,0 +1,13 @@
+package com.pragma.emazon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmazonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmazonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pragma/emazon/EmazonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pragma.emazon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmazonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
